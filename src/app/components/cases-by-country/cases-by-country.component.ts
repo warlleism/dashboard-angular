6b5755933250626fd5a7325dc8e65a32af5e7fd4
Warllei Martins <warllei.s.martins@hotmail.com>
2023-05-22 16:50:03 -0300

    correções

--- src/app/components/cases-by-country/cases-by-country.component.ts
+++ src/app/components/cases-by-country/cases-by-country.component.ts
@@ -14,7 +14,7 @@ interface ICases {
 
 export class CasesByCountryComponent {
 
-  typeCases: boolean = false
+  typeCases: boolean = true
 
   cases: ICases[] = [
     { country: 'Spa', percentage: 40, qtd: "286,308" },
@@ -28,8 +28,24 @@ export class CasesByCountryComponent {
     { country: 'Fra', percentage: 25, qtd: "188,625" },
   ]
 
+  infos: ICases[] = [
+    { country: 'Spa', percentage: 90, qtd: "286,308" },
+    { country: 'Brz', percentage: 10, qtd: "498,440" },
+    { country: 'Ind', percentage: 10, qtd: "181,827" },
+    { country: 'Rus', percentage: 20, qtd: "396,575" },
+    { country: 'Usa', percentage: 10, qtd: "1,816,820" },
+    { country: 'UK', percentage: 25, qtd: "272,826" },
+    { country: 'Ita', percentage: 30, qtd: "232,664" },
+    { country: 'Ger', percentage: 50, qtd: "183,294" },
+    { country: 'Fra', percentage: 55, qtd: "188,625" },
+  ]
+
   sortArray() {
     this.cases = this.cases.sort((a, b) => b.percentage - a.percentage);
+  }
+
+  sortArray2() {
+    this.infos = this.infos.sort((a, b) => b.percentage - a.percentage);
   }
 
   handlerTotalCases(value: boolean) {
@@ -38,6 +54,7 @@ export class CasesByCountryComponent {
 
   ngOnInit(): void {
     this.sortArray()
+    this.sortArray2()
   }
 
 }
